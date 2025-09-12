b2fa33aeb43e0f0f42ca703a977249b1c0706d5b
AB <64165784+ABPaladin@users.noreply.github.com>
2025-09-12 20:12:03 +0300

    main start

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Welcome!</h1>
+    <div class="card">
+      <p>
+      Input for ip
+    </p>  
+    
+    <button type="button">Submit</button>
+      </div>
+    <p class="read-the-docs">
+      a
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
