05e4a27ab339b38ac5a7a2aee67c812075779fd7
PunkyIANG <tsoorckanukris@gmail.com>
2025-09-13 13:25:14 +0300

    order the drop table statements to make sense

--- DB/db.sql
+++ DB/db.sql
@@ -1,16 +1,26 @@
+drop table if exists checklists;
+
+drop table if exists automatic_check_audit_rows;
+drop table if exists automatic_check_audit_headers;
+drop table if exists automatic_checks;
+drop table if exists company_ips;
+
+drop table if exists incidents;
+drop table if exists incident_statuses;
+drop table if exists companies;
+
+
 create table companies (
-    id int primary key generated always as identity,
-    name text not null,
-    password text not null unique
+                           id int primary key generated always as identity,
+                           name text not null,
+                           password text not null unique
 );
-DROP TABLE companies;
 
 create table incident_statuses (
-    id int primary key generated always as identity,
-    name text not null unique,
-    status_order int not null unique
+                                   id int primary key generated always as identity,
+                                   name text not null unique,
+                                   status_order int not null unique
 );
-DROP TABLE incident_statuses
 
 insert into incident_statuses values (default, 'INITIAL', 0);
 insert into incident_statuses values (default, 'UPDATED', 1);
@@ -19,37 +29,34 @@ insert into incident_statuses values (default, 'FINAL', 2);
 
 
 create table incidents (
-    id int primary key generated always as identity,
-    company_id int not null references companies,
-    
-    first_response text not null,
-    first_response_timestamp timestamptz not null default now(),
-    second_response text,
-    second_response_timestamp timestamptz,
-    third_response text,
-    third_response_timestamp timestamptz,
-    
-    is_critical bool not null,
-    is_initially_averted bool not null,
-    repeat_incident_id int null references incidents,
-    status int not null references incident_statuses  
+                           id int primary key generated always as identity,
+                           company_id int not null references companies,
+
+                           first_response text not null,
+                           first_response_timestamp timestamptz not null default now(),
+                           second_response text,
+                           second_response_timestamp timestamptz,
+                           third_response text,
+                           third_response_timestamp timestamptz,
+
+                           is_critical bool not null,
+                           is_initially_averted bool not null,
+                           repeat_incident_id int null references incidents,
+                           status int not null references incident_statuses
 );
-DROP TABLE incidents;
 
 -----------------------------
 
 create table company_ips(
-    id int primary key generated always as identity,
-    company_id int not null references companies,
-    url text
+                            id int primary key generated always as identity,
+                            company_id int not null references companies,
+                            url text
 );
-DROP TABLE company_ips;
 
 create table automatic_checks(
-    id int primary key generated always as identity,
-    name text not null unique
+                                 id int primary key generated always as identity,
+                                 name text not null unique
 );
-DROP TABLE automatic_checks;
 
 insert into automatic_checks values(default, 'Ports');
 insert into automatic_checks values(default, 'TLS');
@@ -57,30 +64,27 @@ insert into automatic_checks values(default, 'HTTP/HTTPS');
 insert into automatic_checks values(default, 'Vulns');
 
 create table automatic_check_audit_headers(
-    id int primary key generated always as identity,
-    company_ip_id int not null references company_ips,
-    datetime timestamptz not null default(now())
+                                              id int primary key generated always as identity,
+                                              company_ip_id int not null references company_ips,
+                                              datetime timestamptz not null default(now())
 );
-DROP TABLE automatic_check_audit_headers;
 
 create table automatic_check_audit_rows(
-    id int primary key generated always as identity,
-    automatic_check_audit_header_id int not null references automatic_check_audit_headers,
-    automatic_check_id int not null references automatic_checks,
-    passed bool,
-    comment text null,
-    unique (automatic_check_audit_header_id, automatic_check_id)
+                                           id int primary key generated always as identity,
+                                           automatic_check_audit_header_id int not null references automatic_check_audit_headers,
+                                           automatic_check_id int not null references automatic_checks,
+                                           passed bool,
+                                           comment text null,
+                                           unique (automatic_check_audit_header_id, automatic_check_id)
 );
-DROP TABLE automatic_check_audit_rows;
 
 -----------------------------
 
 create table checklists(
-    id int primary key generated always as identity,
-    company_ip_id int not null references company_ips,
-    datetime timestamptz not null default now()
-    
+                           id int primary key generated always as identity,
+                           company_ip_id int not null references company_ips,
+                           datetime timestamptz not null default now()
+
     --all the questions and their answers
 );
-DROP TABLE checklists;
 
