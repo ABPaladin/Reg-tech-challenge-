4ceabc36ba16c32566a45b194ca5aea3764373d1
PunkyIANG <tsoorckanukris@gmail.com>
2025-09-14 05:23:50 +0300

    debug log

--- DOTNET BACKEND/Application/Services/CompanyLoginService.cs
+++ DOTNET BACKEND/Application/Services/CompanyLoginService.cs
@@ -1,4 +1,5 @@
-﻿using Application.Interfaces;
+﻿using System.Text.Json;
+using Application.Interfaces;
 using AutoMapper;
 using Domain.DTO;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +13,8 @@ public class CompanyLoginService(PgContext context, IMapper mapper) : ICompanyLo
     public async Task<CompanyRegisterResponseDTO> Register(CompanyRegisterRequestDTO requestDto)
     {
         var company = mapper.Map<Company>(requestDto);
+        
+        Console.WriteLine(JsonSerializer.Serialize(company));
         
         context.Companies.Add(company);
         await context.SaveChangesAsync();
